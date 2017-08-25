d004196fbc61c2be902ae0b6eb4b6c7951b7ba8d
syam4869 <syam4869@icloud.com>
2017-08-26 00:37:24 +0900

    トークン変更

--- public/index.php
+++ public/index.php
@@ -18,12 +18,15 @@ foreach ($json_object->events as $event) {
 
         //スロットの作成
         //キーワード抽出
-        if(strrpos($event->getText(),'今日の天気')==true){
-            api_post_request($event->replyToken, '雨');//post
+        if(strrpos($event->getText(),'天気')==true){
+            api_post_request($event->replyToken, '雨です。');//post
         }else if(strrpos($event->getText(),'記念')==true){
             api_post_request($event->replyToken, '');//post
         }
         api_post_request($event->replyToken, '');//post
+
+
+
 
   //Beaconメッセージ
     }else if('beacon' == $event->type){
@@ -35,7 +38,8 @@ foreach ($json_object->events as $event) {
 //アプリにポストするメソッド（現在テキストメッセージのみ返す仕様）
 function api_post_request($token, $message) {
     $url = 'https://api.line.me/v2/bot/message/reply';
-    $channel_access_token = 'TzygpCcTzSCDAuQqhP4Q5D/wNfweFepCkeIFeCfPCo/8lhH5oovCCYO3dcDwAaQFPofPsEdnEvNPn++0VUTcpxQgX7/kMxiazAWGc4+WIpEAARox2aRLwwm7855PJ6Unwg9I3pqXYbbZD9jnkV67OAdB04t89/1O/w1cDnyilFU=';
+    //$channel_access_token = 'TzygpCcTzSCDAuQqhP4Q5D/wNfweFepCkeIFeCfPCo/8lhH5oovCCYO3dcDwAaQFPofPsEdnEvNPn++0VUTcpxQgX7/kMxiazAWGc4+WIpEAARox2aRLwwm7855PJ6Unwg9I3pqXYbbZD9jnkV67OAdB04t89/1O/w1cDnyilFU=';
+    $channel_access_token = 'Bt66b/PbFie+VJ9+WRCWuNTjVBThsbxzenJP61pbvkO0GXW7U+NyhavKozGGl5Q8PofPsEdnEvNPn++0VUTcpxQgX7/kMxiazAWGc4+WIpEdzM5E5B/fsxTsCNZjdZZYvtz69Ql/gXP5ooJwnyq3dQdB04t89/1O/w1cDnyilFU=';
     $headers = array(
         'Content-Type: application/json',
         "Authorization: Bearer {$channel_access_token}"
